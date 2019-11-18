f60a78ad306a655b7dcc4c4340242350686d0b84
zyj19970725 <55928057+zyj19970725@users.noreply.github.com>
2019-11-18 14:36:28 -0600

    j

--- Shinny App/app.R
+++ Shinny App/app.R
@@ -51,126 +51,125 @@ row.names(Ntable) = NULL
 colnames(Ntable) = 'NOTICE !'
 
 
-
 alert2="We can't offer this part based on the reviews collected."
 Ptable =  data.frame(alert2)
 row.names(Ptable) = NULL
 colnames(Ptable) = 'NOTICE !'
 
-att2=function(business_id){
+sug2=function(business_id){
   business_summary=att_par$dessert[att_par$business_id==business_id]
   if (business_summary == 'False'){
-    return("Dessert: Please provide food that are good for desserts. Your estimated rating will increase by 0.16 by doing this.")
+    return("Please provide food that are good for desserts. Your estimated rating will increase by 0.16 by doing this.")
   }else{
-    return("Dessert: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }
 }
 
-att3=function(business_id){
+sug3=function(business_id){
   business_summary=att_par$latenight[att_par$business_id==business_id]
   if (business_summary == 'False'){
-    return("Latenight Meal: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }else{
-    return("Latenight Meal: It is unnecessary to improve your performance on latenight meal.")
+    return("It is unnecessary to improve your performance on latenight meal.")
   }
 }
 
-att4=function(business_id){
+sug4=function(business_id){
   business_summary=att_par$dinner[att_par$business_id==business_id]
   if (business_summary == 'False'){
-    return("Dinner Meal: Please improve your performance on dinner meal. Your estimated rating will increase by 0.07 by doing this.")
+    return("Please improve your performance on dinner meal. Your estimated rating will increase by 0.07 by doing this.")
   }else{
-    return("Dinner Meal: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }
 }
 
-att5=function(business_id){
+sug5=function(business_id){
   business_summary=att_par$brunch[att_par$business_id==business_id]
   if (business_summary == 'False'){
-    return("Brunch Meal: Please improve your performance on brunch meal. Your estimated rating will increase by 0.25 by doing this.")
+    return("Please improve your performance on brunch meal. Your estimated rating will increase by 0.25 by doing this.")
   }else{
-    return("Brunch Meal: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }
 }
 
-att6=function(business_id){
+sug6=function(business_id){
   business_summary=att_par$Caters[att_par$business_id==business_id]
   if (business_summary == 'False'){
-    return("Caters: Please provide caters. Your estimated rating will increase by 0.26 by doing this.")
+    return("Please provide caters. Your estimated rating will increase by 0.26 by doing this.")
   }else{
-    return("Caters: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }
 }
 
-att7=function(business_id){
+sug7=function(business_id){
   business_summary=att_par$NoiseLevel[att_par$business_id==business_id]
   if (business_summary == "'quiet'"){
-    return("Noise Level: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }else{
-    return("Noise Level: Please keep your environment quiet.")
+    return("Please keep your environment quiet.")
   }
 }
 
-att8=function(business_id){
+sug8=function(business_id){
   business_summary=att_par$RestaurantsTableService[att_par$business_id==business_id]
   if (business_summary == 'False'){
-    return("Table Service: Please provide or improve your table service. Your estimated rating will increase by 0.21 by doing this.")
+    return("Please provide or improve your table service. Your estimated rating will increase by 0.21 by doing this.")
   }else{
-    return("Table Service: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }
 }
 
-att9=function(business_id){
+sug9=function(business_id){
   business_summary=att_par$HasTV[att_par$business_id==business_id]
   if (business_summary == 'False'){
-    return("TV: Please provide TV in your restaurant. Your estimated rating will increase by 0.13 by doing this.")
+    return("Please provide TV in your restaurant. Your estimated rating will increase by 0.13 by doing this.")
   }else{
-    return("TV: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }
 }
 
-att10=function(business_id){
+sug10=function(business_id){
   business_summary=att_par$Alcohol[att_par$business_id==business_id]
   if (business_summary == "'full_bar'"){
-    return("Alcohol: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }else{
-    return("Alcohol: Please provide wine in your restaurant. Your estimated rating will increase by 0.09 by doing this.")
+    return("Please provide wine in your restaurant. Your estimated rating will increase by 0.09 by doing this.")
   }
 }
 
-att11=function(business_id){
+sug11=function(business_id){
   business_summary=att_par$RestaurantsDelivery[att_par$business_id==business_id]
   if (business_summary == 'False'){
-    return("Delivery: Please provide or improve your delivery. Your estimated rating will increase by 0.12 by doing this.")
+    return("Please provide or improve your delivery. Your estimated rating will increase by 0.12 by doing this.")
   }else{
-    return("Delivery: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }
 }
 
-att12=function(business_id){
+sug12=function(business_id){
   business_summary=att_par$BusinessAcceptsCreditCards[att_par$business_id==business_id]
   if (business_summary == 'True'){
-    return("Payment Methods: Please improve your credit card supporting service.")
+    return("Please improve your credit card supporting service.")
   }else{
-    return("Payment Methods: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }
 }
 
-att13=function(business_id){
+sug13=function(business_id){
   business_summary=att_par$WheelchairAccessible[att_par$business_id==business_id]
   if (business_summary == 'True'){
-    return("Wheel Chair: Please forbid wheel chair in your restaurant.")
+    return("Please forbid wheel chair in your restaurant.")
   }else{
-    return("Wheel Chair: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }
 }
 
-att14=function(business_id){
+sug14=function(business_id){
   business_summary=att_par$park[att_par$business_id==business_id]
   if (business_summary == '0'){
-    return("Parking lot: If possible, please provide parking lots. Your estimated rating will increase by 0.08 by doing this.")
+    return("If possible, please provide parking lots. Your estimated rating will increase by 0.08 by doing this.")
   }else{
-    return("Parking lot: There's no need to make a change here.")
+    return("There's no need to make a change here.")
   }
 }
 
@@ -191,7 +190,7 @@ im=function(business_id){
 ui <- fluidPage(
   headerPanel('Suggestions for Business Owners'),
   sidebarPanel(
-    textInput(inputId = 'Business', label =  'Business ID', '1Dfx3zM-rW4n-31KeC8sJg'),
+    textInput(inputId = 'Business', label =  'Business ID', 'voZnDQs6Hs3YpNcS-9TALg'),
     helpText("Please input your business ID."),
     submitButton("Submit")
   ),
@@ -206,20 +205,34 @@ ui <- fluidPage(
                imageOutput("myimage")
       ),
       tabPanel("suggestions based on attribute",
-               h4(textOutput("att1")),
-               h4(textOutput("att2")),
-               h4(textOutput("att3")),
-               h4(textOutput("att4")),
-               h4(textOutput("att5")),
-               h4(textOutput("att6")),
-               h4(textOutput("att7")),
-               h4(textOutput("att8")),
-               h4(textOutput("att9")),
-               h4(textOutput("att10")),
-               h4(textOutput("att11")),
-               h4(textOutput("att12")),
-               h4(textOutput("att13")),
-               h4(textOutput("att14"))
+               h3(textOutput("att1")),
+               h4(textOutput("sug1")),
+               h3(textOutput("att2")),
+               h4(textOutput("sug2")),
+               h3(textOutput("att3")),
+               h4(textOutput("sug3")),
+               h3(textOutput("att4")),
+               h4(textOutput("sug4")),
+               h3(textOutput("att5")),
+               h4(textOutput("sug5")),
+               h3(textOutput("att6")),
+               h4(textOutput("sug6")),
+               h3(textOutput("att7")),
+               h4(textOutput("sug7")),
+               h3(textOutput("att8")),
+               h4(textOutput("sug8")),
+               h3(textOutput("att9")),
+               h4(textOutput("sug9")),
+               h3(textOutput("att10")),
+               h4(textOutput("sug10")),
+               h3(textOutput("att11")),
+               h4(textOutput("sug11")),
+               h3(textOutput("att12")),
+               h4(textOutput("sug12")),
+               h3(textOutput("att13")),
+               h4(textOutput("sug13")),
+               h3(textOutput("att14")),
+               h4(textOutput("sug14"))
       ),
       tabPanel("positive parts in reviews",
                DT::dataTableOutput("pos_review")
@@ -231,7 +244,6 @@ ui <- fluidPage(
   )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
     output$about1 = renderText({
       paste("This app is designed to offer suggestions for business owners of Mexican restaurants to improve their ratings on Yelp.") 
@@ -242,102 +254,200 @@ server <- function(input, output) {
     output$about3 = renderText({
       paste("The second panel is a heat map about the attributes which can mostly improve your rating. The third panel is our suggestions based on all attributes which have significant influence on rating and the rest of the panels are your negative and positive aspects according to the consumers' reviews.")
     })
+    output$sug1 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste("Please provide intimate, hipster, divey or trendy ambience. Your estimated rating will increase by 0.49 if you provide intimate ambience, increase by 0.25 if you provide hipster ambience, increase by 0.22 if you provide divey ambience and increase by 0.25 if you provide trendy ambience.")
+      } else {
+        paste("")
+      }
+    })
+    output$sug2 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug2(input$Business))
+      } else {
+        paste("")
+      }
+    })
+    output$sug3 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug3(input$Business))
+      } else {
+        paste("")
+      }
+    })
+    output$sug4 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug4(input$Business))
+      } else {
+        paste("")
+      }
+    })
+    output$sug5 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug5(input$Business))
+      } else {
+        paste("")
+      }
+    })
+    output$sug6 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug6(input$Business))
+      } else {
+        paste("")
+      }
+    })
+    output$sug7 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug7(input$Business))
+      } else {
+        paste("")
+      }
+    })
+    output$sug8 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug8(input$Business))
+      } else {
+        paste("")
+      }
+    })
+    output$sug9 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug9(input$Business))
+      } else {
+        paste("")
+      }
+    })
+    output$sug10 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug10(input$Business))
+      } else {
+        paste(" ")
+      }
+    })
+    output$sug11 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug11(input$Business))
+      } else {
+        paste(" ")
+      }
+    })
+    output$sug12 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug12(input$Business))
+      } else {
+        paste(" ")
+      }
+    })
+    output$sug13 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug13(input$Business))
+      } else {
+        paste(" ")
+      }
+    })
+    output$sug14 = renderText({
+      if (input$Business %in% att_par$business_id){
+        paste(sug14(input$Business))
+      } else {
+        paste(" ")
+      }
+    })
     output$att1 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste("Ambiance: Please provide intimate, hipster, divey or trendy ambience. Your estimated rating will increase by 0.49 if you provide intimate ambience, increase by 0.25 if you provide hipster ambience, increase by 0.22 if you provide divey ambience and increase by 0.25 if you provide trendy ambience.")
+        paste("Ambience:")
       } else {
         paste(alert)
       }
     })
     output$att2 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att2(input$Business))
+        paste("Dessert:")
       } else {
         paste("")
       }
     })
     output$att3 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att3(input$Business))
+        paste("Latenight Meal:")
       } else {
         paste("")
       }
     })
     output$att4 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att4(input$Business))
+        paste("Dinner Meal:")
       } else {
         paste("")
       }
     })
     output$att5 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att5(input$Business))
+        paste("Brunch Meal: ")
       } else {
         paste("")
       }
     })
     output$att6 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att6(input$Business))
+        paste("Caters: ")
       } else {
         paste("")
       }
     })
     output$att7 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att7(input$Business))
+        paste("Noise Level: ")
       } else {
         paste("")
       }
     })
     output$att8 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att8(input$Business))
+        paste("Table Service: ")
       } else {
         paste("")
       }
     })
     output$att9 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att9(input$Business))
+        paste("TV: ")
       } else {
         paste("")
       }
     })
     output$att10 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att10(input$Business))
+        paste("Alcohol:")
       } else {
-        paste(" ")
+        paste("")
       }
     })
     output$att11 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att11(input$Business))
+        paste("Delivery: ")
       } else {
-        paste(" ")
+        paste("")
       }
     })
     output$att12 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att12(input$Business))
+        paste("Payment Methods: ")
       } else {
-        paste(" ")
+        paste("")
       }
     })
     output$att13 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att13(input$Business))
+        paste("Wheel Chair: ")
       } else {
-        paste(" ")
+        paste("")
       }
     })
     output$att14 = renderText({
       if (input$Business %in% att_par$business_id){
-        paste(att14(input$Business))
+        paste("Parking lot: ")
       } else {
-        paste(" ")
+        paste("")
       }
     })
     output$pos_review = DT::renderDataTable({
@@ -358,12 +468,19 @@ server <- function(input, output) {
         DT::datatable(neg_review(input$Business),options = list(paging = FALSE),caption = 'Your negative parts wrt reviews:')
       }
     })
-    output$myimage <- renderImage({
+    output$myimage = renderImage({
+    if (input$Business %in% att_par$business_id == F){
+      list(src = 'alert.jpg',
+           contentType = 'image/jpg',
+           width = 700,
+           height = 200)
+    } else {
       list(src = im(input$Business),
            contentType = 'image/jpg',
            width = 600,
            height = 500)
-    }, deleteFile = FALSE)
+    }
+    },deleteFile = FALSE)
 }
 
 # Run the application 
